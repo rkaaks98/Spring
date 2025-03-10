fc35fb3161f68d8fc85a6b677e0d3961cddf400a
wnstj050505 <wnstj050505@gmail.com>
2025-03-10 13:03:58 +0900

    ch02

--- ch02/src/main/java/org/example/App.java
+++ ch02/src/main/java/org/example/App.java
@@ -13,6 +13,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * 날짜 : 2025/03/10
  * 이름 : 김준서
  * 내용 : ch02. Spring Ioc/DI 실습하기
+ * 
+ * @Configuration
+ *  - 애플리케이션을 구성하는 빈(객체)을 스프링 컨테이너에 등록하기 위한 설정 클래스 어노테이션
+ * 
+ * @Bean
+ *  - 컨테이너에 등록하기 위한 빈 설정 어노테이션
+ *  - 사용자 정의 클래스 등록 어노테이션
+ *  
+ * @ComponentScan
+ *  - basePackage로 시작하는 패키지내의 빈을 스캔해서 컨테이너에 등록시키는 어노테이션
+ *  - 스캔 대상 컴포넌트는 @Component(@Service, @Repository) 어노테이션 선언해야 됨
+ *  
+ * @Autowired
+ *  - 컨테이너에 등록되어 있는 빈을 주입하는 어노테이션
+ *  - 이름 또는 클래스 타입으로 매칭된 빈을 주입
+ *
  */
 public class App 
 {
